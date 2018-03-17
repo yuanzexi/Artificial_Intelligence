07919ba3e57c453fb868798d55e70c54f794ce20
yuanzexi <yuanzexi63@gmail.com>
2018-03-16 23:14:49 -0400

    tracking

---
+++ pacman/multiagent/a.py
@@ -0,0 +1,9 @@
+import math
+#    info, retri, eva, metr, acc, to, gym, uni, heal
+a = (1+math.log(0.5))*math.log(2.5)
+b = (1+math.log(1.0/3)*math.log(2.5))
+c = (1+math.log(1.0/3))*math.log(5.0/3)
+q = (a, a,0,0,0,0,0,0,0)
+d1 = (b, b, c, 0,0,0,0,0,0)
+
+print (1+math.log(0.5))*math.log(2.5)
